aea07073433f67de3f0901ff8b77204b1c10c6f5
Gianni Mijna <kickoffjacks444@gmail.com>
2025-05-28 02:49:41 +0200

    progressie

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header(){
+return(
+    <Header>
+        <h1> Welkom Bij GridArea</h1>
+        <nav>
+        </nav>
+    </Header>
+)
+
+}
+
+export default Header;
