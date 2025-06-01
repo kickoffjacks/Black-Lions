7a1678fbbeb12929049e685a6c428f0694f11f8c
Gianni Mijna <kickoffjacks444@gmail.com>
2025-06-01 23:29:32 +0200

    Toevoeging van de secties

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -6,10 +6,26 @@ function Process(){
         <div className="processContainer">
 
             <h1 className="processTitle">Ons Process</h1>
-          <div className="processBox1"></div>
-          <div className="processBox1"></div>
-          <div className="processBox1"></div>
-          <div className="processBox1"></div>
+          <div className="processBox1">
+            <h2
+            className="processboxText">Fase: 1 </h2>
+            <div></div>
+          </div>
+          <div className="processBox1">
+            <h2
+            className="processboxText">Fase: 2 </h2>
+            <div></div>
+          </div>
+          <div className="processBox1">
+            <h2
+            className="processboxText">Fase: 3 </h2>
+            <div></div>
+          </div>
+          <div className="processBox1">
+            <h2
+            className="processboxText">Fase: 4 </h2>
+            <div></div>
+          </div>
         </div>
     );
 }
