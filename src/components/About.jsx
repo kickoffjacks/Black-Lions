95d66b961bc0dc6cc0601f2cd3beb812b9f3cf7c
Gianni Mijna <kickoffjacks444@gmail.com>
2025-06-01 22:40:21 +0200

    Slim Fris, Modern toegevoegd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import './styles/About.css';
+import NawiFoto from '../assets/Nawi-Foto.png'; // Assuming you have an image of Nawaaz in the assets folder
 
+//over ons Section
 function About() {
     return (
         <div className="aboutContainer">
             <div className="aboutContent">
 
-                <h1 style={{ color: "white" }}>Over ons</h1>
+                <h1 className="overonsTitle">Slim, Fris, Modern.</h1>
 
 
-                <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', height: '100%' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', height: '110%' }}>
                     <div className="overonsTextContainerHalve">
 
 
                         <div className='TextBox'>
-                            <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
+                                <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
                             <p></p>
                         </div>
                         <div className='TextBox'>
-                            <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
+                                <h2 className="TextBoxTitle">Wat voor apps maken wij?</h2>
                             <p></p>
                         </div>
                         <div className='TextBox'>
-                            <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
+                                <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
                             <p></p>
                         </div>
                     </div>
@@ -33,20 +35,25 @@ function About() {
 
                         <div className="contactonsGridContent">
 
-                            <div className='overonsGridHeader'>Contact?
-                            </div>
-
-                            <div className="avatar"></div>
-                            <div className="contentText">
-
-                                <h3>Meer weten over ons?</h3>
-                                <p>Neem contact met ons op via de onderstaande knop.</p>
-                            </div>
 
 
-                            <div className="buttonContent">
-                                <button className="contactButton">Contact</button>
-                            </div>
+
+                                <div className='overonsGridHeader'>Head business development
+                                </div>
+
+                                <div className="avatar">
+                                    <img className="avatarImg" src={NawiFoto} alt="fotovanNawaaz" />
+                                </div>
+                                <div className="contentText">
+
+                                    <h3 className="meerwetenText">Meer weten?</h3>
+                                    <p>Neem contact met ons.</p>
+                                </div>
+
+
+                                <div className="buttonContent">
+                                    <button className="contactButton">Contact</button>
+                                </div>
 
 
                         </div>
