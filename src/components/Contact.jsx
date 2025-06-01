f09ad996d1529c781745136f3093ae3f1731ca87
Opura-System <mitchelsullivan3751@gmail.com>
2025-06-01 21:53:09 +0200

    ik heb de bel en mail ons knoppen aangepast

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './styles/Contact.css';
 
-
 function Contact() {
     return (
         <div className="contactContainer">
             <div className="contactContent">
-
-
                 <h2 className="CTAtext">Klaar voor verandering?</h2>
-                <div className="buttonContainer">
-                    <button className="belonsButton">Bel ons</button>
-                    <button className="belonsButton mailonsButton">Mail ons</button>
+                <div className="button-container">
+                    <button className="belOns">
+                        Bel ons
+                    </button>
+                    <button className="mailOns">
+                        Mail ons
+                    </button>
                 </div>
             </div>
         </div>
     );
-};
+}
 
 export default Contact;
