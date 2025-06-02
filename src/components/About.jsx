fed8a2a6a9d30c525334a2795976fb098654ee05
Gianni Mijna <kickoffjacks444@gmail.com>
2025-06-03 01:54:13 +0200

    verder gegaan met over ons en process

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,16 +16,25 @@ function About() {
 
 
                         <div className='TextBox'>
-                                <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
-                            <p></p>
+                            <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
+                            <p style={{color: 'white',}}>🔍 Betrouwbare kwaliteit: We leveren altijd een strak werkende website of app.
+                                ⚡ Snelle ontwikkeling: Geen maanden wachten. We bouwen snel en doordacht.
+                                💬 Persoonlijk contact: Je spreekt met mensen, niet met helpdesks.
+                                🎯 Focus op resultaat: Alles wat we maken, is gericht op jouw doelen.</p>
                         </div>
                         <div className='TextBox'>
-                                <h2 className="TextBoxTitle">Wat voor apps maken wij?</h2>
-                            <p></p>
+                            <h2 className="TextBoxTitle">Wat voor apps maken wij?</h2>
+                            <p style={{color: 'white',}}>📱 Mobiele apps: Voor iOS & Android, intuïtief en snel.
+                                🛍️ Webshops & platforms: Van marketplaces tot boekingssystemen.
+                                📊 Dashboards & tools: Voor intern gebruik of klantenportalen.
+                                🔌 API-integraties: We koppelen alles — van betalingssystemen tot datafeeds</p>
                         </div>
                         <div className='TextBox'>
-                                <h2 className="TextBoxTitle">Waarom bedrijven voor ons kiezen?</h2>
-                            <p></p>
+                            <h2 className="TextBoxTitle">Hoe werken wij samen?</h2>
+                            <p style={{color: 'white',}}>🤝 Samen sparren: Jij kent je doelgroep, wij vertalen dat naar techniek.
+                                🧠 Heldere stappen: Van ontwerp tot oplevering, je weet altijd waar we zijn.
+                                🔄 Flexibel: We denken met je mee als dingen tussendoor veranderen.
+                                📦 Afgeleverd & klaar: Geen half werk. Je krijgt een product dat werkt én schaalbaar is.</p>
                         </div>
                     </div>
 
@@ -38,22 +47,22 @@ function About() {
 
 
 
-                                <div className='overonsGridHeader'>Head business development
-                                </div>
+                            <div className='overonsGridHeader'>Head business development
+                            </div>
 
-                                <div className="avatar">
-                                    <img className="avatarImg" src={NawiFoto} alt="fotovanNawaaz" />
-                                </div>
-                                <div className="contentText">
+                            <div className="avatar">
+                                <img className="avatarImg" src={NawiFoto} alt="fotovanNawaaz" />
+                            </div>
+                            <div className="contentText">
 
-                                    <h3 className="meerwetenText">Meer weten?</h3>
-                                    <p>Neem contact met ons.</p>
-                                </div>
+                                <h3 className="meerwetenText">Meer weten?</h3>
+                                <p>Neem contact met ons.</p>
+                            </div>
 
 
-                                <div className="buttonContent">
-                                    <button className="contactButton">Contact</button>
-                                </div>
+                            <div className="buttonContent">
+                                <button className="contactButton">Contact</button>
+                            </div>
 
 
                         </div>
